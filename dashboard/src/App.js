f92762961b107fba5656f25670bd470955bcffe7
Williams Velasquez <Williams.Velasquez@jala.university>
2023-03-08 21:52:58 -0400

    Add first layout

---
+++ dashboard/src/App.js
@@ -0,0 +1,29 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from './scenes/global/Topbar';
+import Sidebar from "./scenes/global/Sidebar";
+import Dashborard from "./scenes/dashboard";
+
+
+function App() {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar/>
+              <Routes>
+                <Route path="/" element={<Dashborard />} />
+              </Routes>        
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
