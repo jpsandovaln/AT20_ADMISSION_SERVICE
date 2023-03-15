45ad0280eecae530336c9ca0e563ed830ffd2f68
Lorena Ramirez <Lorena.Ramirez@jala.university>
2023-03-15 15:30:46 -0500

    Add files whit temporal data

--- dashboard/src/scenes/meeting/index.jsx
+++ dashboard/src/scenes/meeting/index.jsx
@@ -6,8 +6,18 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import Autocomplete from '@mui/material/Autocomplete';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import timeZone from './timezone';
+import host from './hosts';
+import Checkbox from '@mui/material/Checkbox';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import guests from './guests';
 
-
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 export default function NewMeeting() {
     return (
@@ -46,24 +56,24 @@ export default function NewMeeting() {
                     />
                 </div>
             </Box>
-            <br></br>Schedule<br></br>
+            <br></br>Schedule<br></br><br></br>
             <Box component="form"
                 sx={{
-                '& .MuiTextField-root': { m: 1, width: '25ch' },
+                '& .MuiTextField-root': { m: 1, width: '50ch' },
                 }}>
-                <br></br>Date
+                Date
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DemoContainer components={['DatePicker']}>
-                        <DatePicker label="Pick a Date" />
+                        <DatePicker label="Select a Date" />
                     </DemoContainer>
                 </LocalizationProvider>                
             </Box>
-            
+            <Stack spacing={2} direction="row">
             <Box component="form"
                 sx={{
-                '& .MuiTextField-root': { m: 1, width: '25ch' },
+                '& .MuiTextField-root': { m: 1, width: '23ch' },
                 }}>
-                <br></br>Start
+                Start
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DemoContainer components={['TimePicker']}>
                         <TimePicker label="Star time" />
@@ -73,20 +83,72 @@ export default function NewMeeting() {
 
             <Box component="form"
                 sx={{
-                '& .MuiTextField-root': { m: 1, width: '25ch' },
+                '& .MuiTextField-root': { m: 1, width: '23ch' },
                 }}>
-                <br></br>Final time
+                Final time
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DemoContainer components={['TimePicker']}>
                         <TimePicker label="End time" />
                     </DemoContainer>
                 </LocalizationProvider>            
             </Box>
+            </Stack>
             <Box>
-     
+                Time Zone
+                <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={timeZone}
+                    sx={{ m:1, width: '50ch' }}
+                    renderInput={(params) => <TextField {...params} label="Time Zone" />}
+                />
             </Box>
-            
+            <br></br>Participants<br></br><br></br>
+            <Box>
+                Host
+                <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={host}
+                    sx={{ m:1, width: '50ch' }}
+                    renderInput={(params) => <TextField {...params} label="Select Host" />}
+                />   
+            </Box>
 
+            <Box>
+                Guests
+                <Autocomplete
+                    multiple
+                    id="checkboxes-tags-demo"
+                    options={guests}
+                    disableCloseOnSelect
+                    getOptionLabel={(option) => option.name}
+                    renderOption={(props, option, { selected }) => (
+                        <li {...props}>
+                            <Checkbox
+                                icon={icon}
+                                checkedIcon={checkedIcon}
+                                style={{ marginRight: 8 }}
+                                checked={selected}
+                            />
+                            {option.name}
+                        </li>
+                    )}
+                    style={{ width: '50ch' }}
+                    sx={{ m:1, width: '50ch' }}
+                    renderInput={(params) => (
+                        <TextField {...params} label="Select Guests" placeholder=" " />
+                        )}
+                />
+            </Box>
+            <Box>
+                <br></br>
+                <Stack spacing={2} direction="row">
+                    <Button variant="contained" color='secondary' href="#outlined-buttons">Save</Button>
+                    <Button variant="contained" color='primary'>Cancel</Button>
+                </Stack>
+                <br></br>Organized by: Pepito Perez<br></br><br></br>
+            </Box>
         </div>
     );
   }
