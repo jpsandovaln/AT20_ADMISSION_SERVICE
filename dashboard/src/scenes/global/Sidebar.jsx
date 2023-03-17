835cfc42f4c3ebdd05e10220e0e339b1e896df34
Lorena Ramirez <Lorena.Ramirez@jala.university>
2023-03-17 18:40:42 -0500

    solve conflicts

--- dashboard/src/scenes/global/Sidebar.jsx
+++ dashboard/src/scenes/global/Sidebar.jsx
@@ -144,22 +144,7 @@ const Sidebar = () => {
               color={colors.grey[300]}
               sx={{ m: "15px 0 5px 20px" }}
             >
-              Meeting
-            </Typography>
 
-            <Item
-              title="New meeting"
-              to="/meeting"
-              icon={<VideoCallIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-
-            <Typography
-                variant="h5"
-                color={colors.grey[300]}
-                sx={{ m: "15px 0 5px 20px" }}
-            >
               Meeting
             </Typography>
 
