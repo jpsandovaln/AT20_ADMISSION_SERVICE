9b8af32bb4d6a857686e2e4b30e9d556d62b68c3
Carolina Zegarra <carolina.zegarra.60111@usalesiana.edu.bo>
2023-03-17 01:04:00 -0400

    Structure of the pages section updated with their index

---
+++ dashboard/src/scenes/test_sector/spatial/index.jsx
@@ -0,0 +1,13 @@
+import {Box } from "@mui/material";
+import Header from "../../../components/header";
+
+const SpatialTest = () => {
+    
+  return (
+    <Box m="20px">
+      <Header title="SPATIAL TEST" subtitle="Test" />
+    </Box>
+  );
+};
+
+export default SpatialTest;
