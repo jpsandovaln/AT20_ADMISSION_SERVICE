4c5f117ad9daf3d2d29cc1436f9b4effa88c2a2a
Giovanni Blanco <Giovanni.Blanco@jala.university>
2023-03-21 12:58:40 -0400

    Initial version of waiting room

---
+++ dashboard/src/scenes/waiting/index.jsx
@@ -0,0 +1,21 @@
+/*
+@node_command.js Copyright (c) 2023 Jalasoft
+2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+Av. General Inofuentes esquina Calle 20, Edificio Union No 1376, La Paz, Bolivia All rights reserved
+This software is the confidential and proprietary information of
+Jalasoft, Confidential Information "). You shall not
+disclose such Confidential Information and shall use it only in
+accordance with the terms of the license agreement you entered into with Jalasoft
+*/
+
+import React from 'react';
+
+export default function WaitingRoom() {
+    return (
+        <div >
+            <h4 style={{textAlign: "center"}}>WAITING ROOM</h4>
+            <h2 style={{textAlign: "center"}}>Your meeting is schedulet to:</h2>
+            <h2 style={{textAlign: "center"}}>Thursday, March 28, 6:00 PM, 2023</h2>
+        </div>
+    );
+  }
