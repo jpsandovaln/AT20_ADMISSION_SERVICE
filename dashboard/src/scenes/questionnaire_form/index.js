e2e5b8e30171f68a07f9ace5c425f2390f490da1
Adalid Perez <Adalid.Perez@jala.university>
2023-03-23 15:11:36 -0400

    Add copyright

--- dashboard/src/scenes/questionnaire_form/index.js
+++ dashboard/src/scenes/questionnaire_form/index.js
@@ -1,3 +1,12 @@
+/*
+@node_command.js Copyright (c) 2023 Jalasoft
+2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+Av. General Inofuentes esquina Calle 20, Edificio Union No 1376, La Paz, Bolivia All rights reserved
+This software is the confidential and proprietary information of
+Jalasoft, Confidential Information "). You shall not
+disclose such Confidential Information and shall use it only in
+accordance with the terms of the license agreement you entered into with Jalasoft
+*/
 import { Box, Button, TextField, InputLabel, Select, MenuItem } from "@mui/material";
 import { Formik } from "formik";
 import Typography from "@mui/material/Typography";
