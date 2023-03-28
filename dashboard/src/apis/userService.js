067bfccda2cac65399e63e8e3c5b57877fe05a14
Charles Flores <Charles.Flores@jala.university>
2023-03-28 08:19:42 -0400

    Code fixing: userService.js file

--- dashboard/src/apis/userService.js
+++ dashboard/src/apis/userService.js
@@ -1,30 +1,30 @@
-// import axios from "axios";
-// // const API_URL = "http://localhost:8080";
-// const API_URL = "http://localhost:9090";
+import axios from 'axios';
+// const API_URL = 'http://localhost:8080';
+const API_URL = 'http://localhost:9090';
 
-// export const createUser = async (userData) => {
-//     const response = await axios.post(`${API_URL}/users`, userData);
-//     return response.data;
-// };
+export const createUser = async (userData) => {
+    const response = await axios.post(`${API_URL}/users`, userData);
+    return response.data;
+};
 
-// export const getUsers = async () => {
-//     const response = await axios.get(`${API_URL}/users`);
-//     return response.data;
-// };
+export const getUsers = async () => {
+    const response = await axios.get(`${API_URL}/users`);
+    return response.data;
+};
 
-// export const getUserById = async (id) => {
-//     const response = await axios.get(`${API_URL}/users/${id}`);
-//     return response.data;
-// };
+export const getUserById = async (id) => {
+    const response = await axios.get(`${API_URL}/users/${id}`);
+    return response.data;
+};
 
-// export const updateUserById = async (id, userData) => {
-//     const response = await axios.put(`${API_URL}/users/${id}`, {
-//         name: userData.name // Only send the updated name property
-//     });
-//     return response.data;
-// };
+export const updateUserById = async (id, userData) => {
+    const response = await axios.put(`${API_URL}/users/${id}`, {
+        name: userData.name // Only send the updated name property
+    });
+    return response.data;
+};
 
-// export const deleteUserById = async (id) => {
-//     const response = await axios.delete(`${API_URL}/users/${id}`);
-//     return response.data;
-// };
+export const deleteUserById = async (id) => {
+    const response = await axios.delete(`${API_URL}/users/${id}`);
+    return response.data;
+};
