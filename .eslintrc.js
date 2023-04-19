1ce91b20da27921035221e50354cc3917e3c82b5
Percy Rodrigo Fernandez Villarroel <fernandezvillarroel@gmail.com>
2023-04-19 16:21:39 -0400

    merge my team pull requests

---
+++ .eslintrc.js
@@ -0,0 +1,89 @@
+/*module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'plugin:react/recommended',
+        'standard',
+        'plugin:react/jsx-runtime',
+        'eslint:recommended'
+    ],
+    overrides: [],
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest'
+    },
+    plugins: [
+        'react'
+    ],
+    rules: {
+        indent: [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            process.env.NODE_ENV === 'prod' ? 'unix' : 'windows'
+        ],
+        quotes: [
+            'error',
+            'single'
+        ],
+        semi: [
+            'error',
+            'always'
+        ],
+        'brace-style': [
+            'error',
+            '1tbs'
+        ],
+        'lines-between-class-members': [
+            'error',
+            'always'
+        ],
+        'space-in-parens': [
+            'error',
+            'never'
+        ],
+        'space-before-blocks': [
+            'error',
+            'always'
+        ],
+        'padded-blocks': [
+            'error',
+            'never'
+        ],
+        curly: [
+            'error',
+            'all'
+        ],
+        'comma-spacing': [
+            'error',
+            {
+                before: false,
+                after: true
+            }
+        ],
+        'keyword-spacing': [
+            'error',
+            {
+                after: true
+            }
+        ],
+        'semi-spacing': [
+            'error',
+            {
+                before: false,
+                after: true
+            }
+        ],
+        'no-multiple-empty-lines': 'error',
+        'no-trailing-spaces': 'error',
+        camelcase: 'error',
+        'space-infix-ops': 'error',
+        'no-console': 2
+    }
+};
+*/
---
+++ src/scenes/interviews/english/index.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { Box } from '@mui/material';
+import Header from '../../../components/header';
+
+const InterviewEnglish = () => {
+    return (
+        // eslint-disable-next-line react/react-in-jsx-scope
+        <Box m='50px'>
+            <Header title='ENGLISH INTERVIEW' subtitle='Interview' />
+        </Box>
+    );
+};
+
+export default InterviewEnglish;
