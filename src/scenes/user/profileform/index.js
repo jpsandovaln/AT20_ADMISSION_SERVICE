77eb8b0481ed18ef71bfe08b81c87f695c306b3e
esthervasquez1 <Esther.Vasquez@jala.university>
2023-04-23 12:38:05 -0400

    Separed css and updated ProfileForm

--- src/scenes/user/profileform/index.js
+++ src/scenes/user/profileform/index.js
@@ -1,28 +1,28 @@
+// eslint-disable-next-line
 import { Box, Button, TextField, Select, MenuItem } from "@mui/material";
 import { Formik } from "formik";
 //import * as yup from "yup";
 //import axios from "axios";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useState } from "react"
-import {useMutation, useQuery} from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
 import { CREATE_USER, UPLOAD_IMAGE } from "../../../graphql/user";
 import Header from '../../../components/header';
 import { GET_ROLES } from "../../../graphql/role";
+import "./profileFormStyle.css";
 
 const initialValues = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    contact: '',
-    rol: ''
+  firstName: '',
+  lastName: '',
+  email: '',
+  contact: '',
+  rol: ''
 };
 
-
-
-export default function Form () {
+export default function Form() {
   const isNonMobile = useMediaQuery("(min-width:600px)");
 
-  const { data: rolesData, loading: rolesLoading, error: rolesError } = useQuery(GET_ROLES );
+  const { data: rolesData, loading: rolesLoading, error: rolesError } = useQuery(GET_ROLES);
 
   const [user, setUser] = useState({
     globalID: "",
@@ -43,28 +43,23 @@ export default function Form () {
   const [createUser, { loading, error }] = useMutation(CREATE_USER, {
     onCompleted: () => {
       alert('User created successfully!');
-      setUser({ firstName: '', lastName:'', email: '', phone:'', role: '', photo:'' });
+      setUser({ firstName: '', lastName: '', email: '', phone: '', role: '', photo: '' });
     },
   });
 
   const [singleUpload] = useMutation(UPLOAD_IMAGE);
   const [newImage, setnewImage] = useState(null);
 
-const [userPhoto, setUserPhoto] = useState(null);
+  const [userPhoto, setUserPhoto] = useState(null);
 
-const handlePhotoChange = (e) => {
-  setUserPhoto(e.target.files[0]);
-  setnewImage(e.target.files[0]);
-};
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [fileUploaded, setFileUploaded] = useState(false);
 
-const handlePhotoUpload = async () => {
- const { data } = await singleUpload({
-    variables: {
-      file: newImage,
-    },
-  });
-  return data.singleUpload;
-};
+  const handlePhotoChange = (e) => {
+    setUserPhoto(e.target.files[0]);
+    setnewImage(e.target.files[0]);
+    setFileUploaded(true); // Set fileUploaded to true when a file is uploaded
+  };
 
   if (loading) return <p>Loading...</p>
   if (error) return `Submission error! ${error.message}`;
@@ -94,26 +89,16 @@ const handlePhotoUpload = async () => {
   };
 
   return (
-
-    <Box m="20px">
-    <Header title="CREATE USER" subtitle="NEW USER" />
-      <Formik
-        onSubmit={handleSubmit}
-      >
+    <Box className="form-container">
+      <Header title="CREATE USER" subtitle="NEW USER" />
+      <Formik onSubmit={handleSubmit}>
         {({
           errors,
           touched,
           isSubmitting,
         }) => (
           <form onSubmit={handleSubmit}>
-            <Box
-              display="grid"
-              gap="30px"
-              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-              sx={{
-                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-              }}
-            >
+            <Box className="form-grid">
               <TextField
                 fullWidth
                 variant="filled"
@@ -123,7 +108,7 @@ const handlePhotoUpload = async () => {
                 name="firstName"
                 error={!!touched.name && !!errors.name}
                 helperText={touched.name && errors.name}
-                sx={{ gridColumn: "span 2" }}
+                className="form-grid-span2"
               />
               <TextField
                 fullWidth
@@ -134,7 +119,7 @@ const handlePhotoUpload = async () => {
                 name="lastName"
                 error={!!touched.lastName && !!errors.lastName}
                 helperText={touched.lastName && errors.lastName}
-                sx={{ gridColumn: 'span 2' }}
+                className="form-grid-span2"
               />
               <TextField
                 fullWidth
@@ -156,25 +141,46 @@ const handlePhotoUpload = async () => {
                 name="phone"
                 error={!!touched.name && !!errors.name}
                 helperText={touched.name && errors.name}
-                sx={{ gridColumn: "span 2" }}
+                className="form-grid-span2"
               />
-              <Box sx={{ gridColumn: "span 2" }}>
-              <Select
-                    fullWidth
-                    variant="filled"
-                    label="Role"
-                    name="role"
-                    onChange={handleChange}
-                    value={user.role}
-                    error={!!touched.role && !!errors.role}
-                    >
-                  {rolesData && rolesData.roles.map((role) => (
-                  <MenuItem key={role._id} value={role._id}>{role.name}</MenuItem>
+              <Box className="form-grid-span2">
+                <Select
+                  fullWidth
+                  variant="filled"
+                  label="Role"
+                  name="role"
+                  onChange={handleChange}
+                  value={user.role}
+                  error={!!touched.role && !!errors.role}
+                >
+                  {rolesData &&
+                    rolesData.roles.map((role) => (
+                      <MenuItem key={role._id} value={role._id}>
+                        {role.name}
+                      </MenuItem>
                     ))}
-                  </Select>
+                </Select>
               </Box>
             </Box>
-            <Box display="flex" justifyContent="end" mt="20px">
+            <Box className="form-end">
+              <div className="custom-file-container">
+                <Button
+                  className="custom-file-input"
+                  component="label"
+                  color="secondary"
+                  variant="contained"
+                  disabled={isSubmitting}
+                >
+                  {selectedFile || "Choose File"}
+                  <input
+                    type="file"
+                    name="photo"
+                    value={user.photo}
+                    onChange={handlePhotoChange}
+                  />
+                </Button>
+                <span className="no-file-chosen">{!fileUploaded ? "No file chosen" : "Uploaded file"}</span> 
+              </div>
               <Button
                 type="submit"
                 color="secondary"
@@ -184,18 +190,10 @@ const handlePhotoUpload = async () => {
                 {isSubmitting ? "Creating..." : "Create New User"}
               </Button>
             </Box>
-            <Box>
-            <input
-              type="file"
-              name="photo"
-              value={user.photo}
-              onChange={handlePhotoChange}
-              />
-            </Box>
           </form>
         )}
       </Formik>
     </Box>
   );
-};
-export { initialValues, Form};
+}
+export { initialValues, Form };
